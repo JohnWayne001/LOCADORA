df8daff5f329c06d1fdb38ebe5b01e53ce587710
John <johnwaynegtmelo@gmail.com>
2025-09-09 13:57:55 -0300

    terminei temporariamente as classes

--- classes.py
+++ classes.py
@@ -1,52 +1,16 @@
-class Locadora:
-    def __init__(self, nome, localizacao):
-        self.__nome = nome
-        self.__localizacao = localizacao
-        self.__itens = []
-        self.__clientes = []
-
-    def cadastrarCliente(self, nome, cpf):
-        self.__clientes.append(nome)
-
-    def cadastrarItem(self, codigo, titulo):
-        self.
-
-    def listarClientes():
-        pass
-
-    def listarItem():
-        pass
-
-
-class Cliente:
-    def __init__(self, nome, cpf):
-        self.__nome = nome
-        self.__cpf = cpf
-        self.__itensLocados = []
-
-    def locar(self, item: Item):
-
-
-    def devolver(Item):
-        pass
-
-    def listaritens():
-        pass
-
-
-
 class Item:
     def __init__(self, codigo, titulo):
         self.__codigo = codigo
         self.__titulo = titulo
-        disponibilidade = True
+        self.__disponibilidade = True
 
-    def alugar():
-        disponibilidade = False
+    def alugar(self):
+        if self.__disponibilidade:
+            disponibilidade = False
 
-    def devolver():
-        disponibilidade = True
-
+    def devolver(self):
+        if not self.__disponibilidade:
+            disponibilidade = True
 
 
 class Filme(Item):
@@ -56,11 +20,52 @@ class Filme(Item):
         self.__genero = genero
         self.__duracao = duracao
 
-
-
 class Jogo(Item):
     def __init__(self, codigo, titulo, plataforma, faixaEtaria):
         self.__codigo = codigo
         self.__titulo = titulo
         self.__plataforma = plataforma
         self.__faixaEtaria = faixaEtaria
+
+
+class Cliente:
+    def __init__(self, nome, cpf):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__itensLocados = []
+
+    def locar(self, item: Item):
+        if self.__disponibilidade:
+            self.__disponibilidade = False
+        
+
+    def devolver(self, item: Item):
+        if self.__disponibilidade:
+            self.__disponibilidade = True
+
+    def listaritens(self):
+        for i in self.__itensLocados:
+            print(f"Livro: {i}")
+
+
+class Locadora:
+    def __init__(self, nome, localizacao):
+        self.__nome = nome
+        self.__localizacao = localizacao
+        self.__itens = []
+        self.__clientes = []
+
+    def cadastrarCliente(self, clientes: Cliente):
+        self.__clientes.append(clientes)
+
+    def cadastrarItem(self, item: Item):
+        self.__itens.append(item)
+        self.__itens.append(Item)
+
+    def listarClientes(self):
+        for i in self.__clientes:
+            print(f"Clientes: {i}")
+
+    def listarItem(self):
+        for i in self.__itens:
+            print(f"Itens: {i}")
